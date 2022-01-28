36685e8f32b4d793acf8052287806e97ddccefe8
Raphael <raphael.campagnac@live.fr>
2022-01-28 11:48:45 +0100

    migration (creation des tablzs)

---
+++ Migrations/20220128104546_InitialCreate.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LaCantine.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utilisateur",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumTel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Solde = table.Column<double>(type: "float", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateDeNaissance = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilisateur", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commandes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Statut = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrixTotal = table.Column<double>(type: "float", nullable: false),
+                    UtilisateurId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commandes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Commandes_Utilisateur_UtilisateurId",
+                        column: x => x.UtilisateurId,
+                        principalTable: "Utilisateur",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Libelle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prix = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommandesID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Menu_Commandes_CommandesID",
+                        column: x => x.CommandesID,
+                        principalTable: "Commandes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plats",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Libelle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prix = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommandesID = table.Column<int>(type: "int", nullable: true),
+                    MenuID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plats", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Plats_Commandes_CommandesID",
+                        column: x => x.CommandesID,
+                        principalTable: "Commandes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Plats_Menu_MenuID",
+                        column: x => x.MenuID,
+                        principalTable: "Menu",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produits_Allergenes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Produit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlatsID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produits_Allergenes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Produits_Allergenes_Plats_PlatsID",
+                        column: x => x.PlatsID,
+                        principalTable: "Plats",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_UtilisateurId",
+                table: "Commandes",
+                column: "UtilisateurId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menu_CommandesID",
+                table: "Menu",
+                column: "CommandesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plats_CommandesID",
+                table: "Plats",
+                column: "CommandesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plats_MenuID",
+                table: "Plats",
+                column: "MenuID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produits_Allergenes_PlatsID",
+                table: "Produits_Allergenes",
+                column: "PlatsID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produits_Allergenes");
+
+            migrationBuilder.DropTable(
+                name: "Plats");
+
+            migrationBuilder.DropTable(
+                name: "Menu");
+
+            migrationBuilder.DropTable(
+                name: "Commandes");
+
+            migrationBuilder.DropTable(
+                name: "Utilisateur");
+        }
+    }
+}
