db4881101da429b58fa2bb0c6e1f4c97ec891754
Raphael Campagnac <rcampagnac@cegid.com>
2022-07-21 20:12:13 +0200

    tests unitaires plats

--- LaCantineTest/Controllers/PlatsControllerTest.cs
+++ LaCantineTest/Controllers/PlatsControllerTest.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace LaCantineTest
 {
@@ -14,35 +16,69 @@ namespace LaCantineTest
     public class PlatsControllerTest
     {
         private LaCantineContext context;
+        private PlatsController controller;
 
         public PlatsControllerTest()
         {
             var options =
                 new DbContextOptionsBuilder<LaCantineContext>()
-                    .UseInMemoryDatabase(databaseName: "WeatherDb")
+                    .UseInMemoryDatabase(databaseName: "PlatDb")
                     .Options;
             context = new LaCantineContext(options);
-            var entity = new Plats
-            {
-                
-            };
 
-            context.Plats.Add(entity);
-            context.SaveChanges();
+            context.Plats.Add(new Plats
+            {
+                id = 1,
+                name = "steak",
+                desc = "miam",
+                photo = "",
+                prix = 5
+            });
+            context.Plats.Add(new Plats
+            {
+                id = 2,
+                name = "salade",
+                desc = "berk",
+                photo = "",
+                prix = 2
+            });
+
+            controller = new PlatsController(context);
         }
 
-/*        [TestMethod]
-        public async Task Getplat_date_ok()
+        [TestMethod]
+        public async Task Get_Plats()
         {
-            var repository = new DBPlatsRepository(context);
-            var service = new PlatsService(repository);
-            var controller = new PlatsController(null,service);
 
+            var result = await controller.GetPlats();
+            //vérification type de la réponse
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<Plats>>));
+            //vérification du contenu de la réponse
+            Assert.AreEqual(context.Plats.Count(), result.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task Get_Plats_by_id()
+        {
             var result = await controller.GetPlats(1);
-            Assert.IsInstanceOfType(result,typeof(ActionResult<Plats>));
-            var okObject = result as ActionResult<Plats>;
-          
-            Assert.AreEqual(new DateTime(2001, 02, 03),okObject.Value.Date);
-        }*/
+            //vérification type de la réponse
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Plats>));
+            //vérification du contenu de la réponse
+            Assert.AreEqual("steak", result.Value.name);
+        }
+
+        [TestMethod]
+        public async Task PUT_Plats()
+        {
+            var steak = await controller.GetPlats(1);
+            steak.Value.name = "lasagnes";
+            await controller.PutPlats(1,steak.Value);
+            var result = await controller.GetPlats(1);
+
+            //vérification type de la réponse
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Plats>));
+            //vérification du contenu de la réponse
+            Assert.AreEqual("lasagnes", result.Value.name);
+        }
     }
 }
